f5644e95e102e0dd81e714c20723f99146af31fc
Atreya Bhat <agbhat@wpi.edu>
2024-04-30 21:58:34 -0400

    update

--- lib/axisangle2rot.m
+++
@@ -1,8 +0,0 @@
-function R = axisangle2rot(omega,theta)
-   
-    K = skew(omega);
-    
-    %Rodrigues formula
-    R = eye(3) + sin(theta) * K + (1 - cos(theta)) * (K^2);
-
-end
--- lib/xyzrpy_to_transformation.m
+++
@@ -1,9 +0,0 @@
-function T = xyzrpy_to_transformation(path)
-    % Create the rotation matrix using the RPY angles
-    R = eul2rotm([path(6), path(5), path(4)] * pi/180, 'ZYX');
-
-    % Create the homogeneous transformation matrix
-    T = eye(4);
-    T(1:3, 1:3) = R;
-    T(1:3, 4) = [path(1); path(2); path(3)];
-end
